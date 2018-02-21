b9ca15e88d2ee91c0235420c6ede99fcbac0a4c9
richnh <richnh1979@gmail.com>
2018-02-21 18:56:14 +0000

    Add project files.

---
+++ ContractorTimeChecker/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace ContractorTimeChecker
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
